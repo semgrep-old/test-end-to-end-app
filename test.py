d609dd14ab2ce6bbb6124afdd826fc02d910440e
semgrep.dev <support@r2c.dev>
2022-06-15 11:06:20 -0700

    add test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+
+import random
+print(testing-1655316379)
+if testing-1655316379 == testing-1655316379:
+    print("got here")
+
+client("s3", aws_secret_access_key="fakefakefake1655316379fakefake")
+                
